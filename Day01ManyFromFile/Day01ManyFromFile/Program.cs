89970646a01afa80c345c75b6491633435865476
Roberto Panetta <rob.pane@gmail.com>
2018-10-11 16:17:34 -0400

    Finished

--- Day01ManyFromFile/Day01ManyFromFile/Program.cs
+++ Day01ManyFromFile/Day01ManyFromFile/Program.cs
@@ -9,33 +9,33 @@ namespace Day01ManyFromFile
 {
     class Program
     {
-      abstract class Person
+      class Person
         {
             public String Name;
             public int Age;
 
             public virtual string ToString()
             {
-                return Name+" is "+Age+" y/o";
+                return Name+" is a person "+Age+" y/o.";
             }
         }
         class Student : Person
         {
-            String Program;
-            Double GPA;
+            public String Program;
+            public Double GPA;
             public override string ToString()
             {
-                return Name + " is " + Age + " y/o, studing in "+Program+" with a "+GPA+" GPA";
+                return Name + " is a student " + Age + " y/o, studying in "+Program+" with a "+GPA+" GPA.";
             }
         }
 
         class Teacher : Person
         {
-            String Subject;
-            int YOP; // Years Of Experience Teaching
+            public String Subject;
+            public int YOP; // Years Of Experience Teaching
             public override string ToString()
             {
-                return Name + " is " + Age + " y/o, and has been teaching "+Subject+" for "+YOP+" years";
+                return Name + " is a teacher " + Age + " y/o, and has been teaching "+Subject+" for "+YOP+" years.";
             }
         }
         static void Main(string[] args)
@@ -47,14 +47,32 @@ namespace Day01ManyFromFile
                 foreach (string line in lines)
                 {
                     string[] words = line.Split(';');
-                   
-                    //People.Add(e);
+                    if (words[0] == "Person")
+                    {
+                        Person e = new Person() { Name = words[1], Age = int.Parse(words[2]) };
+                        People.Add(e);
+                    }
+                    else if (words[0] == "Student")
+                    {
+                        Person e = new Student() { Name = words[1], Age = int.Parse(words[2]), Program = words[3], GPA = double.Parse(words[4]) };
+                        People.Add(e);
+                    }
+                    else if (words[0] == "Teacher")
+                    {
+                        Person e = new Teacher() { Name = words[1], Age = int.Parse(words[2]), Subject = words[3], YOP = int.Parse(words[4]) };
+                        People.Add(e);
+                    }
+
 
                 }
             }
             catch (IOException ex)
             {
-
+                Console.Write(ex);
+            }
+            foreach (var v in People)
+            {
+                Console.WriteLine(v.ToString());
             }
             Console.ReadKey();
         }
