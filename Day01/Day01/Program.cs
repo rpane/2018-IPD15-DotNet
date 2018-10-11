ba12e68819532d60cbb61998d16d64919d1d9eb5
Roberto Panetta <31354921+rpane@users.noreply.github.com>
2018-10-11 10:40:03 -0400

    ...

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hi {0}, you are {1} y/o", name, age);
+            if(age < 18)
+            {
+                Console.WriteLine("You are not an adult yet");
+            }
+            Console.ReadKey();
+        }
+    }
+}
