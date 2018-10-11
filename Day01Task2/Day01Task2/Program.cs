750c8dacfadfa091a5c2f9d4d0ccf6674dc64a06
Roberto Panetta <31354921+rpane@users.noreply.github.com>
2018-10-11 11:35:16 -0400

    ..

--- Day01Task2/Day01Task2/Program.cs
+++ Day01Task2/Day01Task2/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day01Task2
 {
+    class Person
+    {
+        public string name;
+        public int age;
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Person> People = new List<Person>();
+            try
+            {
+                string[] lines = File.ReadAllLines("people.txt");
+
+                foreach (string line in lines)
+                {
+                    string name =;
+                    int age =;
+                    Person e = new Person() { name = "abc", age = 23 };
+                    People.Add(e);
+                    e.ToString();
+                }
+            }catch(IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            People.Sort();
+            Console.WriteLine("============Sorted by name=============");
+            Console.ReadKey();
+            
         }
+
+       
+        
     }
 }
