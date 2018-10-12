f28697bd988db4c63ad8982268821797fac7eff5
Roberto Panetta <31354921+rpane@users.noreply.github.com>
2018-10-12 11:24:59 -0400

    Update

--- Day01ManyFromFile/Day01ManyFromFile/Person.cs
+++ Day01ManyFromFile/Day01ManyFromFile/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,38 @@ using System.Threading.Tasks;
 namespace Day01ManyFromFile
 {
     class Person
-    {
-        public String Name;
-        public int Age;
+    {      
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        private String _name;
+        public String Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    throw new InvalidDataException("Name too short");
+                }
+                _name = value;
+            }
+        }
+        private int _age;
+        public int Age
+        {
+            get {return _age; }
+            set
+            {
+                if(value <1)
+                {
+                    throw new InvalidDataException("Age needs to be above 0");
+                }
+                _age = value;
+            }
+        }
 
         public virtual string ToString()
         {
@@ -17,9 +47,45 @@ namespace Day01ManyFromFile
         }
     }
     class Student : Person
-    {
-        public String Program;
-        public Double GPA;
+    {  
+        public Student(string name, int age, string program, double gPA) : base(name,age)
+        {
+            Program = program;
+            GPA = gPA;
+        }
+        private String _program;
+        public String Program
+        {
+            get
+            {
+                return _program;
+            }
+            set
+            {
+                if (value.Length <1)
+                {
+                    throw new InvalidDataException("Program name too short");
+                }
+                _program = value;
+            }
+        }
+        private Double _gpa;
+        public Double GPA
+        {
+            get
+            {
+                return _gpa;
+            }
+            set
+            {
+                if(value < 1)
+                {
+                    throw new InvalidDataException("GPA is too low, must be above 20");
+                }
+                _gpa = value;
+            }
+        }
+
         public override string ToString()
         {
             return Name + " is a student " + Age + " y/o, studying in " + Program + " with a " + GPA + " GPA.";
@@ -28,8 +94,44 @@ namespace Day01ManyFromFile
 
     class Teacher : Person
     {
-        public String Subject;
-        public int YOP; // Years Of Experience Teaching
+        public Teacher(string name, int age, string subject, int yOP) : base(name, age)
+        {
+            Subject = subject;
+            YOP = yOP;
+        }
+        private String _subject;
+        public String Subject
+        {
+            get
+            {
+                return _subject;
+            }
+            set
+            {
+                if (value.Length <1)
+                {
+                    throw new InvalidDataException("Subject name too short");
+                }
+                _subject = value;
+            }
+        }
+        private int _yop;
+        public int YOP
+        {
+            get
+            {
+                return _yop;
+            }
+            set
+            {
+                if (value <0)
+                {
+                    throw new InvalidDataException("Teaching years needs to be a positive value");
+                }
+                _yop = value;
+            }
+        }
+
         public override string ToString()
         {
             return Name + " is a teacher " + Age + " y/o, and has been teaching " + Subject + " for " + YOP + " years.";
--- Day01ManyFromFile/Day01ManyFromFile/Program.cs
+++ Day01ManyFromFile/Day01ManyFromFile/Program.cs
@@ -12,41 +12,45 @@ namespace Day01ManyFromFile
         static void Main(string[] args)
         {
             List<Person> People = new List<Person>();
+            String Name;
+            int Age;
+            String Program;
+            Double GPA;
+            String Subject;
+            int YOP;
             try
             {
                 string[] lines = File.ReadAllLines(@"..\..\many.txt");
                 foreach (string line in lines)
                 {
                     string[] words = line.Split(';');
-                    if (words[0] == "Person")
-                    {
-                        Person e = new Person() { Name = words[1], Age = int.Parse(words[2]) };
-                        People.Add(e);
-                    }
-                    else if (words[0] == "Student")
-                    {
-                        Person e = new Student() { Name = words[1], Age = int.Parse(words[2]), Program = words[3], GPA = double.Parse(words[4]) };
-                        People.Add(e);
-                    }
-                    else if (words[0] == "Teacher")
-                    {
-                        Person e = new Teacher() { Name = words[1], Age = int.Parse(words[2]), Subject = words[3], YOP = int.Parse(words[4]) };
-                        People.Add(e);
-                    }
-                    else {
-
-                    }
-
-
+                    
+                        if (words[0] == "Person" && words.Length == 3)
+                        {
+                            Person e = new Person(Name = words[1], Age = int.Parse(words[2]));
+                            People.Add(e);
+                        }
+                        else if (words[0] == "Student" && words.Length == 5)
+                        {
+                            Person e = new Student(Name = words[1], Age = int.Parse(words[2]), Program = words[3], GPA = double.Parse(words[4]));
+                            People.Add(e);
+                        }
+                        else if (words[0] == "Teacher" && words.Length == 5)
+                        {
+                            Person e = new Teacher(Name = words[1], Age = int.Parse(words[2]), Subject = words[3], YOP = int.Parse(words[4]));
+                            People.Add(e);
+                        }                                     
                 }
             }
             catch (IOException ex)
             {
                 Console.Write(ex);
+                
             }
+            
             foreach (var v in People)
             {
-                Console.WriteLine(v.ToString());
+                Console.WriteLine(v.ToString());               
             }
             Console.ReadKey();
         }
