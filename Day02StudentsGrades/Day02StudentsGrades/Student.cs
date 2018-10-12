53ef0a261ca7dd9904f1cfa370dcb0ccc92b1721
Roberto Panetta <31354921+rpane@users.noreply.github.com>
2018-10-12 16:39:15 -0400

    task 3 started

---
+++ Day02StudentsGrades/Day02StudentsGrades/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02StudentsGrades
+{
+    class Student
+    {
+
+        public Student(string name)
+        {
+            _name = name;
+        }
+        private string _name;
+	    public string Name
+        { // no setter
+            get
+            {
+                return _name;
+            }
+        }
+        private List<double> gradeList;
+        
+        public void addGrade(double grade)
+        {
+            gradeList.Add(grade);
+        }
+        
+        public double Average
+        { // no setter
+            get
+            {
+                // compute average here and return
+                return gradeList.Average();
+            }
+        }
+    }
+}
