054fb0ad79692b68a1ec85d98f31674f59f85b6c
Roberto Panetta <31354921+rpane@users.noreply.github.com>
2018-10-17 10:03:43 -0400

    Updated Day4AllInputs

--- Day04AllInputs/Day04AllInputs/MainWindow.xaml.cs
+++ Day04AllInputs/Day04AllInputs/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +24,19 @@ namespace Day04AllInputs
         public MainWindow()
         {
             InitializeComponent();
+            rbBelow18.IsChecked = true;
+
+            
         }
 
         private void btnRegister_Click(object sender, RoutedEventArgs e)
         {
+            //Instead of a String method create a list of petChoices
+            List<string> petList = new List<string>();
+            if (cbCat.IsChecked == true) petList.Add("Cat");
+            if (cbDog.IsChecked == true) petList.Add("Dog");
+            if (cbOther.IsChecked == true) petList.Add("Other");
+            string petStr = string.Join(",", petList);
 
             if (tbName.Text == "")
             {
@@ -34,14 +44,20 @@ namespace Day04AllInputs
                 throw new Exception("Please Enter your name");
             }
             string name = tbName.Text;
-            string text = string.Format("{0};{1};{2};{3};{4: 0.00}°C", name,btnChoice(),petChoice(),cbContinents.Text,tbSliderVal.Text);
-            System.IO.File.AppendAllText(@"..\..\friends.txt", text + Environment.NewLine);
+            string text = string.Format("{0};{1};{2};{3};{4: 0.00}°C", name,btnChoice(),petStr,cbContinents.Text, lblSliderVal.Content.ToString());
+            try
+            {
+                System.IO.File.AppendAllText(@"..\..\friends.txt", text + Environment.NewLine);
+            }catch(IOException E)
+            {
+                MessageBox.Show(this, "Failed to write data to file.\n" + E.Message, "File Error");
+            }
 
             tbName.Text = "";
             slTemp.Value = 25;
             rb18_35.IsChecked = false;
             rb36_.IsChecked = false;
-            rbBelow18.IsChecked = false;
+            rbBelow18.IsChecked = true;
             cbCat.IsChecked = false;
             cbDog.IsChecked = false;
             cbOther.IsChecked = false;
@@ -69,7 +85,9 @@ namespace Day04AllInputs
 
         return Age;
     }
-
+       
+         
+        //Used List in btn Event instead of this method
         public String petChoice()
         {
             String res = "";
@@ -106,6 +124,12 @@ namespace Day04AllInputs
                 res = "No Pet";
             }
             return res;
+        }
+        string selectedAge = "";
+        private void rbBelow18_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            selectedAge = rb.Content.ToString();
         }
     }
 }
