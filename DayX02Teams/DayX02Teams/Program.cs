fbec6d13a742187ee41b844d564d6891838a3e0d
Roberto Panetta <rob.pane@gmail.com>
2018-10-28 16:57:16 -0400

    Teams Completed
    Check catches on Duplicates

--- DayX02Teams/DayX02Teams/Program.cs
+++ DayX02Teams/DayX02Teams/Program.cs
@@ -9,25 +9,63 @@ namespace DayX02Teams
 {
     class Program
     {
-        int gamesWon;
-        int gamesLost;
-        int goalsShot;
+        
         static void Main(string[] args)
         {
-            SortedDictionary< Team, string> fifa = new SortedDictionary<Team, string>();
+            SortedDictionary<string, Team> fifa = new SortedDictionary<string, Team>();
+            fifa.Clear();
             try
             {
-                string[] lineArray = File.ReadAllLines("teams.txt");
+                string[] lineArray = File.ReadAllLines(@"..\..\teams.txt");
+                
                 foreach (string line in lineArray)
                 {
                     string[] split = line.Split(';');
-                
+                    try
+                    {
+                        Team x = new Team(split[0]);
+                        Team y = new Team(split[1]);
+                        fifa.Add(split[0],x);
+                        fifa.Add(split[1], y);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("An element with Key = "+split[0]+" already exists.");
+                    }
+
+                    fifa[split[0]].GoalsShot += int.Parse(split[2]);
+                    fifa[split[1]].GoalsShot += int.Parse(split[3]);
+                    if(int.Parse(split[2])> int.Parse(split[3]))
+                    {
+                        fifa[split[0]].GamesWon++;
+                        fifa[split[1]].GamesLost++;
+                    }
+                    else
+                    {
+                        fifa[split[1]].GamesWon++;
+                        fifa[split[0]].GamesLost++;
+                    }
+
                 }
             }
             catch (IOException ex)
             {
                 Console.WriteLine(ex);
             }
+            foreach(KeyValuePair<string, Team> kvp in fifa)
+            {
+                Console.WriteLine("Team = {0}, Games Won = {1}, Games Lost = {2}, Goals Shot = {3}",
+                kvp.Key, kvp.Value.GamesWon, kvp.Value.GamesLost, kvp.Value.GoalsShot);
+            }
+            Console.WriteLine("----------------------------------Order by Games Won-----------------------");
+            var gamesWon = fifa.OrderByDescending(x => x.Value.GamesWon);
+            foreach( var x in gamesWon)
+            {
+                Console.WriteLine("Team = {0}, Games Won = {1}, Games Lost = {2}, Goals Shot = {3}",
+                x.Key, x.Value.GamesWon, x.Value.GamesLost, x.Value.GoalsShot);
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- DayX02Teams/DayX02Teams/Team.cs
+++ DayX02Teams/DayX02Teams/Team.cs
@@ -13,19 +13,14 @@ namespace DayX02Teams
         private int gamesLost;
         private int goalsShot;
 
-        public Team(string name, int gamesWon, int gamesLost, int goalsShot)
+        public Team(string name)
         {
             Name = name;
-            GamesWon = gamesWon;
-            GamesLost = gamesLost;
-            GoalsShot = goalsShot;
         }
 
         public string Name { get => name; set => name = value; }
         public int GamesWon { get => gamesWon; set => gamesWon = value; }
         public int GamesLost { get => gamesLost; set => gamesLost = value; }
         public int GoalsShot { get => goalsShot; set => goalsShot = value; }
-
-     
     }
 }
